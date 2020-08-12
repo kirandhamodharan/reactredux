77d416516a84cbfd360ddb4683563c079b0dc51a
Kiran <kirandhamodharan@gmail.com>
2020-08-12 12:47:15 -0500

    Updated package versions

--- webpack-config/common.js
+++ webpack-config/common.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const PROJ_DIR = "../../.."
+const PROJ_DIR = ".."
 
 let htmlOptions = {
     template:  './src/static/index.html',
-    title:     "Kiran's Simple React App1",
+    title:     "Kiran's React Redux App",
     filename:  'index.html',
     favicon:   './src/static/images/favicon.ico'
 }
@@ -36,6 +37,11 @@ module.exports = {
     },
     output: {
         filename: 'app.js',
-        path: path.resolve(__dirname + PROJ_DIR, 'build')
-    }
+        path: path.resolve(__dirname, PROJ_DIR, 'build')
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin(htmlOptions)
+    ],
 };
